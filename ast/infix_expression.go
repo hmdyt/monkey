7d857d0cf2df09d3ae7fb7109c59aed3935ccd36
hmdyt <konnyaku108@gmail.com>
2023-01-21 15:53:45 +0900

    arrange file dividing

---
+++ ast/infix_expression.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+	"bytes"
+
+	"github.com/hmdyt/monkey/token"
+)
+
+type InfixExpression struct {
+	Token    token.Token // operator token, for example "+"
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (i InfixExpression) expressionNode()      {}
+func (i InfixExpression) TokenLiteral() string { return i.Token.Literal }
+func (i InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(i.Left.String())
+	out.WriteString(" " + i.Operator + " ")
+	out.WriteString(i.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
