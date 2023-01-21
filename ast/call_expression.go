10ef1ba57f7295016439f7a96d7a8d796e23b359
hmdyt <konnyaku108@gmail.com>
2023-01-21 18:05:32 +0900

    add call expression

---
+++ ast/call_expression.go
@@ -0,0 +1,32 @@
+package ast
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/hmdyt/monkey/token"
+)
+
+type CallExpression struct {
+	Token     token.Token // '(' token
+	Function  Expression  // Identifier or FunctionLiteral
+	Arguments []Expression
+}
+
+func (c CallExpression) expressionNode()      {}
+func (c CallExpression) TokenLiteral() string { return c.Token.Literal }
+func (c CallExpression) String() string {
+	var out bytes.Buffer
+
+	var args []string
+	for _, a := range c.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(c.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
+
+	return out.String()
+}
